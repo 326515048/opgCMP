670f24524e264518daef9dcaf8fd7aa89c0ace53
fan.shijun <326515048@qq.com>
2018-01-16 17:04:01 +0800

    01-16

--- src/main/java/com/opg/jpademo/CdnTraffic/CdnTrafficService.java
+++ src/main/java/com/opg/jpademo/CdnTraffic/CdnTrafficService.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by fan.shijun on 2017-12-04.
@@ -39,4 +40,21 @@ public class CdnTrafficService {
         return cdnTrafficRepository.findAll();
     }
 
+    @RequestMapping(value = "/cdnTraffic/testadd", method = RequestMethod.GET)
+    @ResponseBody
+    public String testadd() {
+        long utctime1 = 1506787200;
+        Random ran1 = new Random(100);
+        for (int i = 0; i < 1000; i++) {
+            CdnTraffic cdnTraffic = new CdnTraffic();
+            cdnTraffic.setCdnCompany("azure");
+            cdnTraffic.setDomainName("live.a");
+            cdnTraffic.setTrafficID(null);
+            cdnTraffic.setTrafficUtcTime(String.valueOf(utctime1));
+            cdnTraffic.setTrafficValue(ran1.nextInt(100));
+            cdnTrafficRepository.save(cdnTraffic);
+            utctime1 = utctime1 + 3600;
+        }
+        return "OK";
+    }
 }
---
+++ src/main/java/com/opg/jpademo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.opg.jpademo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * Created by fan.shijun on 2018-01-15.
+ */
+public class ServletInitializer extends SpringBootServletInitializer {
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(JpAdemo1Application.class);
+}
